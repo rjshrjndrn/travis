65d6dda39979311c51567876ca37c453f0dc66d0
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-08 21:14:45 +0530

    Update

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo $USER_PWD"
-echo $USER_NAME"
 echo "$USER_PWD" | docker login -u "$USER_NAME" --password-stdin
 docker push rjshrjndrn/ansible:2.4.1
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 nc -zv localhost 8000
+echo $USER_PWD"
+echo $USER_NAME"
