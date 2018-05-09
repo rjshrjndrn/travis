2b7901caa823a669be639b85b1a4dd1932a6dc81
rjshrjndrn <rjshrjndrn@gmail.com>
2018-05-09 14:33:31 +0530

    Updated varaible

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$USER_PWD" | docker login -u "$USER_NAME" --password-stdin
+echo "$USER_PWD" | docker login -u "$USER_NAMEs" --password-stdin
 docker push rjshrjndrn/ansible:2.4.1
